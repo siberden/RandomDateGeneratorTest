fbfb578b7ba59c91562e0e23ffb3b61ad17f3243
karac <karacayberkuk@hotmail.com>
2024-10-11 02:37:18 -0400

    Fixed vulnerabilities in dependencies and updated Jackson Databind to version 2.14.2 Updated TestRunner class to use @RunWith(Cucumber.class) instead of @RunWith(CucumberRunner.class) Renamed RandomDateGeneratorSteps class to match file name convention Added OWASP Dependency Check plugin to Maven build to identify and manage vulnerabilities in dependencies

--- src/test/java/StepDefinition/RandomDateGeneratorSteps.java
+++ src/test/java/StepDefinition/RandomDateGeneratorSteps.java
@@ -1,5 +1,6 @@
 package StepDefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
@@ -7,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import utility.BrowserDriver;
 
 public class RandomDateGeneratorSteps {
+
     private WebDriver driver;
 
     public RandomDateGeneratorSteps() {
@@ -18,23 +20,23 @@ public class RandomDateGeneratorSteps {
         driver.get("https://codebeautify.org/generate-random-date");
     }
 
-    @When("I enter {string} in the field")
-    public void i_enter_in_the_field(String value) {
-        driver.findElement(By.name("value")).sendKeys(value);
+    @When("I enter {int} in the field")
+    public void i_enter_in_the_field(int value) {
+        driver.findElement(By.xpath("//*[@id=\"count\"]")).sendKeys(String.valueOf(value));
     }
 
-    @When("I enter {string} as start date")
-    public void i_enter_as_start_date(String startDate) {
-        driver.findElement(By.name("start_date")).sendKeys(startDate);
+    @And("I enter {int} as start date")
+    public void i_enter_as_start_date(int startDate) {
+        driver.findElement(By.xpath("//*[@id=\"start\"]")).sendKeys(String.valueOf(startDate));
     }
 
-    @When("I enter {string} as end date")
-    public void i_enter_as_end_date(String endDate) {
-        driver.findElement(By.name("end_date")).sendKeys(endDate);
+    @And("I enter {int} as end date")
+    public void i_enter_as_end_date(int endDate) {
+        driver.findElement(By.xpath("//*[@id=\"end\"]")).sendKeys(String.valueOf(endDate));
     }
 
-    @When("I click the generate button")
+    @And("I click the generate button")
     public void i_click_the_generate_button() {
-        driver.findElement(By.name("generate_button")).click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/section[2]/div/div[2]/button[1]/span")).click();
     }
 }
--- src/test/java/feature/random_date_generator.feature
+++ src/test/java/feature/random_date_generator.feature
@@ -3,11 +3,11 @@ Feature: Random Date Generator
 
   Scenario Outline: Generate random dates with invalid input
     Given I open the random date generator page
-    When I enter "<value>" in the field
-    And I enter "<start date>" as start date
-    And I enter "<end date>" as end date
+    When I enter <value> in the field
+    And I enter <start_date> as start date
+    And I enter <end_date> as end date
     And I click the generate button
 
     Examples:
-      | value | start date | end date |
+      | value | start_date | end_date |
       | -1   | 2020-01-01 00:00:00 | 2099-12-31 23:59:59 |
